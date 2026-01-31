975904e621578325a0d8c3fcc19a35574496b907
KY233466 <katieyang233@gmail.com>
2026-01-31 17:29:35 -0500

    attempt of next with github actions

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  basePath: "/ky233466",
   webpack: (config) => {
     config.module.rules.push({
       test: /\.(mp4|mov|webm|ogg)$/i,
