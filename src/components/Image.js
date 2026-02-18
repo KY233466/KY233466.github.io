b9cc0369e474474bd402d5fedf52ece636394673
KY233466 <katieyang233@gmail.com>
2026-02-17 20:54:04 -0500

    remove excess files

--- src/components/Image.js
+++
@@ -1,9 +0,0 @@
-import resolveAsset from "../lib/resolveAsset";
-
-function Image({ src, styles }) {
-  const onLoad = () => {
-    console.log("loaded");
-  };
-  return <img className={styles} src={resolveAsset(src)} onLoad={onLoad} />;
-}
-export default Image;
